17307af7d0cd437fa910e8f1f997e26258230c95
Fikiham <laztnamed@gmail.com>
2022-12-26 11:19:09 +0700

    test

---
+++ webpack.mix.js
@@ -0,0 +1,11 @@
+// webpack.mix.js
+
+import { js } from 'laravel-mix';
+
+js('resources/js/app.js', 'public/js')
+    .vue({
+        version: 3,
+    })
+    .postCss('resources/css/app.css', 'public/css', [
+        //
+    ]);
